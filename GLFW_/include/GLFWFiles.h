23b838134d8ee7ddc9a56e17138868f1fc8f3501
glegat08 <glegat@gaming.tech>
2026-02-04 21:54:30 +0100

    [devLG] added comments for main .h files

--- GLFW_/include/GLFWFiles.h
+++ GLFW_/include/GLFWFiles.h
@@ -1,6 +1,11 @@
 #ifndef GLFW_FILES
 #define GLFW_FILES
 
+/*
+	Include here all the new .h files you created.
+	You only need to include this file in your .cpp.
+*/
+
 #include "vulkan/vulkan_raii.hpp"
 #include "glfw/glfw3.h"
 #include "GLFWTest.h"
--- GameLib/include/GameFiles.h
+++ GameLib/include/GameFiles.h
@@ -1,6 +1,11 @@
 #ifndef GAME_FILES
 #define GAME_FILES
 
+/*
+	Include here all the new .h files you created.
+	You only need to include this file in your .cpp.
+*/
+
 #include "vulkan/vulkan_raii.hpp"
 #include "GLFW/glfw3.h"
 #include "GameTest.h"
